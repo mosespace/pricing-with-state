82816ab590d9d45ee98332ec94f5118bf4f59e52
mosespace <kisakyemoses50@gmail.com>
2023-07-08 14:55:10 +0300

    feat: added price data

---
+++ pricingData.js
@@ -0,0 +1,61 @@
+export const monthlyPrice =[
+    {
+        i: '1',
+        title: "Free",
+        price: 49,
+        features:["Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor."],
+        isRecommended: false,
+    },
+    {
+        i: '2',
+        title: "Small Bussiness",
+        price: 99,
+        features:["Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor."],
+        isRecommended: false,
+    },
+    {
+        i: '3',
+        title: "Professional",
+        price: 219,
+        features:["Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor."],
+        isRecommended: true,
+    },
+    {
+        i:'4',
+        title: "Enterprise",
+        price: 419,
+        features:["Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor."],
+        isRecommended: false,
+    }
+]
+
+export const annualPrice =[
+    {
+        i: '1',
+        title: "Free",
+        price: 39,
+        features:["Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor."],
+        isRecommended: false,
+    },
+    {
+        i: '2',
+        title: "Small Bussiness",
+        price: 89,
+        features:["Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor."],
+        isRecommended: false,
+    },
+    {
+        i: '3',
+        title: "Professional",
+        price: 119,
+        features:["Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor."],
+        isRecommended: true,
+    },
+    {
+        i:'4',
+        title: "Enterprise",
+        price: 319,
+        features:["Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor.", "Lorem ipsum dolor."],
+        isRecommended: false,
+    }
+]
